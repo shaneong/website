e520641d9526a4c6d67af1034d38ab4c27939a4f
Gurjot Singh Makkar <gurjotsingh427@gmail.com>
2018-03-04 15:43:55 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './Components/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Gurjot Singh Makkar</h1>
-        </header>
-        <p className="App-intro">
-          React + GitHub pages = Awesomeness!!
-        </p>
+        <NavBar />
       </div>
     );
   }
